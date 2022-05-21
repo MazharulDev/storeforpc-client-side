86f6a3b8586b4a9d84a5a0b092bbe7662e3e55dc
Md Mazharul islam <miforbd@gmail.com>
2022-05-21 22:23:14 +0600

    router tailwind daisyui install  and navbar  added

---
+++ src/pages/Home/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const items = <>
+        <li><Link to='/'>Home</Link></li>
+        {
+            <li><Link to="/dashboard">Dashboard</Link></li>
+        }
+        <li><Link to='/appointment'>Blogs</Link></li>
+        <li><Link to='/contactus'>Contact Us</Link></li>
+
+
+    </>
+    return (
+        <div class="navbar bg-base-100">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {items}
+                    </ul>
+                </div>
+                <Link to='/' class="btn btn-ghost normal-case text-xl font-mono font-bold">StoreForPc</Link>
+            </div>
+            <div class="navbar-end hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    {items}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
