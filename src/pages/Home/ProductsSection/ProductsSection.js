f7e6b59157f13000dd2ad442917bc6d796ccb6cf
Mazharul Islam <mazharulislam.dev@gmail.com>
2022-12-04 20:45:43 +0600

    product load spinner added

--- src/pages/Home/ProductsSection/ProductsSection.js
+++ src/pages/Home/ProductsSection/ProductsSection.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from '../../../shared/Loading/Loading';
 import ProductSection from './ProductSection';
 
 const ProductsSection = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
+        setLoading(true)
         fetch('https://storeforpc.up.railway.app/product', {
             method: 'GET',
             headers: {
@@ -12,7 +15,10 @@ const ProductsSection = () => {
             }
         })
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setLoading(false)
+                setProducts(data)
+            });
     }, [])
 
     return (
@@ -20,14 +26,16 @@ const ProductsSection = () => {
             <div className='flex justify-center'>
                 <h2 className='text-center text-4xl py-5 inline-block border-b-4 border-blue-400 font-bold mb-5'>Choose Product</h2>
             </div>
-            <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-5 my-10'>
-                {
-                    products.slice(0, 6).map(product => <ProductSection key={product._id} product={product}></ProductSection>)
-                }
-            </div>
-            <div className='flex justify-center mb-5'>
-                <Link to='/showallproduct' className="btn btn-outline btn-info">Show All Product</Link>
-            </div>
+            {loading ? <Loading /> : <>
+                <div className='container mx-auto grid grid-cols-1 md:grid-cols-3 gap-5 my-10'>
+                    {
+                        products.slice(0, 6).map(product => <ProductSection key={product._id} product={product}></ProductSection>)
+                    }
+                </div>
+                <div className='flex justify-center mb-5'>
+                    <Link to='/showallproduct' className="btn btn-outline btn-info">Show All Product</Link>
+                </div>
+            </>}
         </div>
     );
 };
