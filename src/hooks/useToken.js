0a808a85e4673f9c2a31cf9f38058f0e831344ec
Md Mazharul Islam <mdmazharulislam.dev@gmail.com>
2023-10-04 08:41:53 +0600

    update footer and my protfolio page information

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
-const useToken = user => {
-    const [userToken, setUserToken] = useState('');
-    useEffect(() => {
-        const email = user?.user?.email
-        const currentUser = { email: email }
-        if (email) {
-            fetch(`${process.env.REACT_APP_SERVER_LINK}/user/${email}`, {
-                method: 'PUT',
-                headers: {
-                    'content-type': 'application/json',
-                    headers: {
-                        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                    }
-                },
-                body: JSON.stringify(currentUser)
-
-            })
-                .then(res => res.json())
-                .then(data => {
-                    const accessToken = data.accessToken;
-                    localStorage.setItem('accessToken', accessToken)
-                    setUserToken(accessToken)
-                })
-        }
-    }, [user])
-    return [userToken]
-}
+import { useEffect, useState } from "react";
+const useToken = (user) => {
+  const [userToken, setUserToken] = useState("");
+  useEffect(() => {
+    const email = user?.user?.email;
+    const currentUser = { email: email };
+    if (email) {
+      fetch(`${process.env.REACT_APP_SERVER_LINK}/user/${email}`, {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+          headers: {
+            authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          },
+        },
+        body: JSON.stringify(currentUser),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const accessToken = data.accessToken;
+          localStorage.setItem("accessToken", accessToken);
+          setUserToken(accessToken);
+        });
+    }
+  }, [user]);
+  return [userToken];
+};
 export default useToken;
--- src/shared/Footer/Footer.js
+++ src/shared/Footer/Footer.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import { BsFacebook } from 'react-icons/bs'
-import { BsTwitter } from 'react-icons/bs'
-import { BsLinkedin } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
+import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
-    return (
-        <>
-            <div className='bg-slate-800 text-white p-5'>
-                <h2 className='text-3xl text-center'>StoreForPc</h2>
-                <p className='text-center my-4'>If you need computer parts, you can easily take them</p>
-                <div className='flex justify-center gap-4'>
-                    <a href="https://web.facebook.com/miforbd"><BsFacebook className='text-2xl hover:scale-105'></BsFacebook></a>
-                    <a href="https://twitter.com/miforbd"><BsTwitter className='text-2xl hover:scale-105'></BsTwitter></a>
-                    <a href="https://www.linkedin.com/in/miforbd/"><BsLinkedin className='text-2xl hover:scale-105'></BsLinkedin></a>
-                    <a href="https://github.com/miforbd"><BsGithub className='text-2xl hover:scale-105'></BsGithub></a>
-                </div>
-                <div>
-
-                </div>
-
-            </div>
-            <div className="text-center bg-black text-white p-2">
-                <p>Copyright &copy; 2022 StoreForPc developed By <a className='text-blue-600' href="https://web.facebook.com/miforbd">Mazharul islam</a></p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-slate-800 text-white p-5">
+        <h2 className="text-3xl text-center">StoreForPc</h2>
+        <p className="text-center my-4">
+          If you need computer parts, you can easily take them
+        </p>
+        <div className="flex justify-center gap-4">
+          <a href="https://web.facebook.com/MazharulDev/">
+            <BsFacebook className="text-2xl hover:scale-105"></BsFacebook>
+          </a>
+          <a href="https://twitter.com/MazharulDev">
+            <BsTwitter className="text-2xl hover:scale-105"></BsTwitter>
+          </a>
+          <a href="https://www.linkedin.com/in/mazharuldev/">
+            <BsLinkedin className="text-2xl hover:scale-105"></BsLinkedin>
+          </a>
+          <a href="https://github.com/MazharulDev">
+            <BsGithub className="text-2xl hover:scale-105"></BsGithub>
+          </a>
+        </div>
+        <div></div>
+      </div>
+      <div className="text-center bg-black text-white p-2">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} StoreForPc developed By{" "}
+          <a
+            className="text-blue-600"
+            href="https://mdmazharulislam-dev.web.app/"
+          >
+            Md Mazharul islam
+          </a>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
